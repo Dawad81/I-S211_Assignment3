7e6d1299b20cf099d9f50129b2ffb05f2913fd49
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-02-14 20:54:52 -0500

    Added main() and RegEx to find jpg|png|gif.

--- Assignment3.py
+++ Assignment3.py
@@ -22,10 +22,35 @@ def processData(downloadcsvfile):
     lines = 0
     imagehits = 0
     for line in reader:
+        #if re.search(r"jpg|png|gif", line[0]):
+        if re.search(r"[^\s]+(?=\.(jpg|gif|png))", line[0]):
+            imagehits += 1
         #print line
         lines += 1
-    return lines
+
+    percentageimagehits = (float(imagehits) / lines) * 100
+    print percentageimagehits 
+    
         
+#if re.search(r"[^\s]+(?=\.(jpg|gif|png))")
+
+#print processData(test1)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", help="Enter a URL to begin.", required=True)
+    args = parser.parse_args()
+    try:
+        csvData = downloadData(args.url)
+    except:
+        print 'An error has occured session terminated.\n\
+        Exiting the program......Good Bye.'
+        raise SystemExit
+    else:
+        processData(csvData)
 
 
-#print pprint(processData(test1))
+
+
+if __name__ == '__main__':
+    main()
