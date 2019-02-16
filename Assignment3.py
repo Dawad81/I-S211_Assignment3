af6b992066a27b1b5cd84bb68acdc3147d1ec245
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-02-16 15:41:37 -0500

    Added Docstrings + edits

--- Assignment3.py
+++ Assignment3.py
@@ -9,17 +9,33 @@ import datetime
 import re
 import urllib2
 
-
-
 url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
 
 
 def downloadData(url=str):
-    """Docstring"""
+    """This function opens an inputed url and return the result to the caller.
+
+    Args:
+
+        url (str): A string that is a URL address.
+
+    Returns:
+
+        file object (various): A file object assigned to the variable
+        downloadcsvfie.
+
+    Example:
+
+        >>> downloadData(
+        'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv')
+        <addinfourl at 140145322261104 whose fp = <socket._fileobject object at
+        0x7f76202c78d0>>
+    """
     downloadcsvfile = urllib2.urlopen(url)
     return downloadcsvfile
 
 test1 = downloadData("http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv")
+
 
 def processData(downloadcsvfile):
     """Docstring"""
@@ -32,8 +48,8 @@ def processData(downloadcsvfile):
     ie = ['Internet Explorer', 0]
     safari = ['Safari', 0]
 
-    dateFormat = '%Y-%m-%d %H:%M:%S'
-    times = {hour: 0 for hour in range(0, 24)}
+    dateformat = '%Y-%m-%d %H:%M:%S'
+    dict_times = {hour: 0 for hour in range(0, 24)}
 
 
     for row in reader:
@@ -54,12 +70,12 @@ def processData(downloadcsvfile):
                 "Chrome", row[2]):
             safari[1] += 1
 
-        date = datetime.datetime.strptime(row[1], dateFormat)
-        times[date.hour] = times[date.hour] + 1
-        sorted_times = sorted(times.items())
+        date = datetime.datetime.strptime(row[1], dateformat)
+        dict_times[date.hour] = dict_times[date.hour] + 1
+        sorted_times = sorted(dict_times.items(), key=lambda kv: kv[1])
         sorted_times.reverse()
 
-    browser_totals = [chrome, ie, safari, firefox]
+    browser_totals = [chrome, safari, ie, firefox]
     popular = 0
     browser_name = ''
     for num in browser_totals:
@@ -73,16 +89,57 @@ def processData(downloadcsvfile):
              'the most used web browser accessing the site with {} '\
              'hits.'.format(percentageimagehits, lines, browser_name, popular)
     print result
-    
-    print '*' * 10,('Extra Credit'),'*' * 10
+
+    print '*' * 10, ('Extra Credit'), '*' * 10
     for time in sorted_times:
         print'Hour %02d has %s hits.' % (time[0], time[1])
 
-
 test2 = processData(test1)
 
+
 def main():
-    """Docstring"""
+    """ This function combines the downloadData() and processData()
+        into a single function to be run on the command line.
+
+        main() dowloads a file from a provided --url, processes the data,
+        then returns the results of the processData() function.
+
+        If an impropper --url is input, an error message is raised and the
+        program exits.
+
+
+    Exsample:
+
+        $ python Assignment3.py --url http://s3.amazonaws.com/cuny-is211-
+        spring2015/weblog.csv
+        Image requests account for 78.77% of todays total 10000 hits.
+        Chrome was the most used web browser accessing the site with 4042 hits.
+        ********** Extra Credit **********
+        Hour 04 has 1813 hits.
+        Hour 01 has 1808 hits.
+        Hour 03 has 1797 hits.
+        Hour 02 has 1795 hits.
+        Hour 00 has 1793 hits.
+        Hour 05 has 994 hits.
+        Hour 23 has 0 hits.
+        Hour 22 has 0 hits.
+        Hour 21 has 0 hits.
+        Hour 20 has 0 hits.
+        Hour 19 has 0 hits.
+        Hour 18 has 0 hits.
+        Hour 17 has 0 hits.
+        Hour 16 has 0 hits.
+        Hour 15 has 0 hits.
+        Hour 14 has 0 hits.
+        Hour 13 has 0 hits.
+        Hour 12 has 0 hits.
+        Hour 11 has 0 hits.
+        Hour 10 has 0 hits.
+        Hour 09 has 0 hits.
+        Hour 08 has 0 hits.
+        Hour 07 has 0 hits.
+        Hour 06 has 0 hits.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="Enter a URL to begin.", required=True)
     args = parser.parse_args()
